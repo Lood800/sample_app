849d7acdafc603fed0dd38243441391be27928fb
Lodewicus Adriaanse <lodewicus@gmail.com>
2015-02-24 11:30:31 +0200

    add password reset and email configuration

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "Title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Litter, the poor man's Twitter"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Litter, the poor man's Twitter"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "About | Litter, the poor man's Twitter"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Litter, the poor man's Twitter"
   end
 
 end
